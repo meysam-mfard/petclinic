f5ef1a77ad44f52735c625ab2fb2a1cd69f8e98e
meysam-mfard <mmohagheghi@gmail.com>
2018-12-31 09:38:56 +0100

    Adding POJOs. Closes #1.

---
+++ src/main/java/meysam/springframework/petclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package meysam.springframework.petclinic.model;
+
+public class PetType {
+
+    private String petType;
+
+    public String getPetType() {
+        return petType;
+    }
+
+    public void setPetType(String petType) {
+        this.petType = petType;
+    }
+}
