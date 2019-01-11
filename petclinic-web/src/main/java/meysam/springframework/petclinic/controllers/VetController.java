059983c118e18b1f8dd72fd6220c389cf08fb27b
meysam-mfard <mmohagheghi@gmail.com>
2019-01-11 18:15:31 +0100

    Added vets list and updated controller.

--- petclinic-web/src/main/java/meysam/springframework/petclinic/controllers/VetController.java
+++ petclinic-web/src/main/java/meysam/springframework/petclinic/controllers/VetController.java
@@ -1,14 +1,25 @@
 package meysam.springframework.petclinic.controllers;
 
+import meysam.springframework.petclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    final private VetService vetService;
 
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
