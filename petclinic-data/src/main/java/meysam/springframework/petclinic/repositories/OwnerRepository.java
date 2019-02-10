c7f70f211d12cfb869ae6a646bdf9e78a346bcc3
meysam-mfard <mmohagheghi@gmail.com>
2019-02-10 18:08:49 +0100

    Added Spring data JPA service for Owner.

--- petclinic-data/src/main/java/meysam/springframework/petclinic/repositories/OwnerRepository.java
+++ petclinic-data/src/main/java/meysam/springframework/petclinic/repositories/OwnerRepository.java
@@ -4,4 +4,7 @@ import meysam.springframework.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    //Query method
+    Owner findByLastName(String lastName);
 }
